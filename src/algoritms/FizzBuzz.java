0e07c743a666a631d45a26cb99d796a4f72fe30e
DianaDennis <kovas06@yahoo.com>
2022-03-16 08:09:32 -0400

    added package

---
+++ src/algoritms/FizzBuzz.java
@@ -0,0 +1,21 @@
+package algoritms;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		//for example
+		//int num [] = {1,2,3,4,5};
+		//her 3 is fizz and 5 is buzz
+		int num = 0;
+		
+		 if (num % 3 ==0){
+			System.out.println(num + "fizz");	
+			
+		}else {
+			System.out.println(num);
+		}
+		
+	}
+
+}
